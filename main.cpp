8b8b4230cea762c4c7aa3622cc7c21fd03a0e4d4
田中泰申 <k022g0122@g.neec.ac.jp>
2024-02-15 10:53:49 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<iostream>
+#include<chrono>
+
+
+using namespace std;
+
+int main(){
+
+	std::string a(100000, 'a');
+
+	auto CopyStart = std::chrono::high_resolution_clock::now();
+	std::string Copystr = a;
+	auto CopyEnd = std::chrono::high_resolution_clock::now();
+	auto CopyDuration = std::chrono::duration_cast<std::chrono::microseconds>(CopyEnd - CopyStart);
+
+
+	auto MoveStart = std::chrono::high_resolution_clock::now();
+	std::string moveStr = std::move(a);
+	auto MoveEnd = std::chrono::high_resolution_clock::now();
+	auto MoveDuration = std::chrono::duration_cast<std::chrono::microseconds>(MoveEnd - MoveStart);
+
+	std::cout << "100,000の文字を移動とコピーで比較しました" << std::endl;
+	std::cout << "コピー" << CopyDuration.count() << "μs" << std::endl;
+	std::cout << "移動" << MoveDuration.count() << "μs" << std::endl;
+
+	//std::chrono
+
+	//std::move(o);
+	//int&& c = 1;
+
+	//Object o;
+	//Test(Test &&right){}
+
+	//Test &operator=(Test &&right){}
+}
